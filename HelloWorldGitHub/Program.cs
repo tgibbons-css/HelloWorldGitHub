bd30174ba4e51f8efc01b1db70f862974a130662
Tom Gibbons <tgibbons@css.edu>
2020-09-24 09:51:43 -0500

    Added more output

--- HelloWorldGitHub/Program.cs
+++ HelloWorldGitHub/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorldGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GITHUB!");
         }
     }
 }
